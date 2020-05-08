b31670dc9aa9939a179bbbabb48e429c28581709
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-05-08 02:19:16 -0400

    Finished lab 1
    Added a summary for the confusion matrix

--- lab1/script.R
+++ lab1/script.R
@@ -1,15 +1,15 @@
 library(ggplot2)
 
 # Establecemos algunas constantes
-filename                       <- "expanded"
-edible.class                   <- "EDIBLE"
-poisonous.class                <- "POISONOUS"
-odor.almond                    <- "ALMOND"
-odor.anise                     <- "ANISE"
-odor.none                      <- "NONE"
+url                            <- "https://raw.githubusercontent.com/Evantastic/analisis12020/master/agaricus-lepiota.data"
+edible.class                   <- "e"
+poisonous.class                <- "p"
+odor.almond                    <- "a"
+odor.anise                     <- "l"
+odor.none                      <- "n"
 
 # Leemos los datos
-mushrooms <- read.table(filename, header = TRUE, sep = ",")
+mushrooms <- read.table(url, header = TRUE, sep = ",")
 
 # Estilo de los graficos
 mynamestheme <- theme(plot.title = element_text(family = "Helvetica", face = "bold", size = (15), hjust = 0.5), 
@@ -53,17 +53,38 @@ n.poisonous <- nrow(poisonous)
 n.poisonous.satisfy.rules <- get.n.after.rules(poisonous, n.poisonous)
 n.edible.satisfy.rules    <- get.n.after.rules(edible, n.edible)
 
-# Tabla de contingencia para resumir los resultados de las pruebas aplicadas
-test.results <- matrix(c(n.edible.satisfy.rules, n.poisonous.satisfy.rules,
-                         n.edible - n.edible.satisfy.rules, n.poisonous - n.poisonous.satisfy.rules),
-                       ncol = 2,
-                       byrow = TRUE)
-colnames(test.results) <- c("Comestibles", "Venenosos")
-rownames(test.results) <- c("Cumplen la regla", "No cumplen la regla")
+
+# Data for the confusion matrix
+tp <- n.poisonous.satisfy.rules
+fp <- n.edible.satisfy.rules
+fn <- n.poisonous - n.poisonous.satisfy.rules
+tn <- n.edible - n.edible.satisfy.rules
+# Matriz de confusion para resumir los resultados de las pruebas aplicadas tiene la siguiente forma
+#                          Venenosos Comestibles
+# Venenosos(Predichos)        tp        fp
+# Comestibles(Predichos)      fn        tn
+confusion.matrix <- matrix(c(tp, fp, fn, tn),
+                           ncol = 2,
+                           byrow = TRUE)
+colnames(confusion.matrix) <- c("Venenosos", "Comestibles")
+rownames(confusion.matrix) <- c("Venenosos (Predicho)", "Comestibles (Predicho)")
+cat("Matriz de confusion", "\n")
+print(confusion.matrix)
+# Summary of the confusion matrix
+confusion.matrix.summary <- matrix(c(tp/(tp + fn), tn/(tn + fp), tp/(tp + fp), tn/(tn + fn),
+                                     fn/(fn + tp), fp/(fp + tn), fp/(fp + tp), fn/(fn + tn), tp/(tp + fn + fp)),
+                                   ncol = 1)
+colnames(confusion.matrix.summary) <- c("Valor")
+rownames(confusion.matrix.summary) <- c("Sensibilidad", "Especificidad", "Precision", "Valor predictivo negativo",
+                                        "Tasas de fallo", "Tasa de falsa alarma", "Tasa de descubrimiento falso",
+                                        "Tasa de omision falsa", "Indice de exito")
+cat("\n","Resumen de la matriz de confusion", "\n")
+print(confusion.matrix.summary)
 
 # Prueba exacta de Fisher
 # Hipotesis nula: Los 3 olores en especifo (ningunom, anis, almendra) no determinan la comestibilidad de un hongo. Las variables son indepenedientes
 # Hipotesis alternativa: Los 3 olores en especifo(ninguno, anis, almendra) determinan la comestibilidad de un hongo. Las variables no son independientes
 
-test <- fisher.test(test.results)
-print(test)
+test <- fisher.test(mushrooms$class, mushrooms$odor, simulate.p.value = TRUE)
+cat("\n","P-valor de la prueba exacta de Fisher","\n")
+cat(test$p.value)
