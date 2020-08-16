6b57de6864b4793e6b294821d0ef26e04e1dbd3e
Vale <valentina.ligueno@gmail.com>
2020-08-16 14:38:45 -0400

    Se quitan variables inutiles

--- lab3/script.R
+++ lab3/script.R
@@ -6,6 +6,13 @@ set.seed(88)
 
 url <- "https://raw.githubusercontent.com/Evantastic/analisis12020/master/agaricus-lepiota.data"
 mushrooms <- read.table(url, header = TRUE, sep = ",")
+
+# Eliminamos atributos que no aportan a la clasificacion
+mushrooms$veil.type <- NULL
+mushrooms$gill.attachment <- NULL
+mushrooms$veil.color <- NULL
+mushrooms$ring.number <- NULL
+
 for(i in 1:ncol(mushrooms)) mushrooms[,i] <- as.factor(mushrooms[,i])
 
 rules = apriori(
