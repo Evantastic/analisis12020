b92083cbb8e0121bd2a55fe11ec6e0f90819143d
Vale <valentina.ligueno@gmail.com>
2020-09-12 19:56:07 -0300

    Arboles

---
+++ lab4/Script.R
@@ -0,0 +1,26 @@
+library("C50")
+library("caret")
+
+# Semilla
+set.seed(88)
+
+url <- "https://raw.githubusercontent.com/Evantastic/analisis12020/master/agaricus-lepiota.data"
+mushrooms <- read.table(url, header = TRUE, sep = ",")
+
+# Eliminamos atributos que no aportan a la clasificacion
+mushrooms$veil.type <- NULL
+mushrooms$gill.attachment <- NULL
+mushrooms$veil.color <- NULL
+mushrooms$ring.number <- NULL
+
+# Convertimos las columnas a factores
+for(i in 1:ncol(mushrooms)) mushrooms[,i] <- as.factor(mushrooms[,i])
+
+training.index = createDataPartition(mushrooms$class, p=0.7)$Resample1
+training.set = mushrooms[training.index, ]
+test.set = mushrooms[-training.index, ]
+tree = C5.0(class ~ ., training.set)
+tree.rules = C5.0(x = training.set[, -8], y = training.set$class, rules = T)
+tree.pred.class = predict(tree, test.set[,-8], type = "class")
+tree.pred.prob = predict(tree, test.set[,-8], type = "prob")
+
